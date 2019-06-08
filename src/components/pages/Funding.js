71a5b1c4f750117b4ea6361b75664c39ba760991
ShaunKolich <employmeinc@gmail.com>
2019-06-07 20:38:23 -0400

    Working on Funding Module using Props

--- src/components/pages/Funding.js
+++ src/components/pages/Funding.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import { Container, Row, Col } from 'fluid-react';
 import '../../less/funding.css'
 
+
 import FundingHeader from '../fundingheader';
+import FundingTop from '../FundingTopInfo';
 
 class Funding extends Component {
     render() {
         return (
             <div>
                 <FundingHeader />
+                <FundingTop/>
             </div>
 
         )
