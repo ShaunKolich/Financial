2ee8063662cb21ff94db2c73f47bf348fbdc5011
ShaunKolich <employmeinc@gmail.com>
2019-06-10 21:39:37 -0400

    Added map started on Contact Form

--- src/components/pages/Partner.js
+++ src/components/pages/Partner.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import { Container, Row, Col } from 'fluid-react'
 import '../../less/partner.css';
 import MapsContent from '../mapContact';
+import ContactInfo from '../ContactInfo';
 
 class Partner extends Component {
     render() {
         return (
 
             <div>
-             <MapsContent/>
+                <MapsContent />
+                
             </div>
         )
     }
