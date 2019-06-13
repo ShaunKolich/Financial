aa6fb39ad6278f8ff52fd75e706d7983d352e246
ShaunKolich <employmeinc@gmail.com>
2019-06-13 19:34:24 -0400

    Testing

--- src/components/pages/Partner.js
+++ src/components/pages/Partner.js
@@ -3,14 +3,14 @@ import { Container, Row, Col } from 'fluid-react'
 import '../../less/partner.css';
 import MapsContent from '../mapContact';
 import ContactInfo from '../ContactInfo';
-import Contact from '../ContactForm';
+import ContactForm from '../ContactForm';
 
 class Partner extends Component {
     render() {
         return (
 
             <div>
-                <Contact/>
+                <ContactForm/>
                 <ContactInfo />
                 <MapsContent />
                 
